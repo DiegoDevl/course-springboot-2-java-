a973a1117ac12f07d83236d1cd581d7294aeafca
Diego <diegosouzafl94@gmail.com>
2020-07-14 22:46:35 -0300

    criando repository User

---
+++ src/main/java/com/projetospring/couse/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.projetospring.couse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetospring.couse.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+
+}
